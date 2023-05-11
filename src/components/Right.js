394db21f5639fd6d281531e85170d964f4ebdc3a
emretokk <emretok35@gmail.com>
2023-05-11 18:49:34 +0300

    sigma background image added

--- src/components/Right.js
+++ src/components/Right.js
@@ -1,5 +1,4 @@
 import { React, Component } from "react";
-import mycoolpic from "../assets/myawesomepic.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
@@ -16,7 +15,50 @@ class Right extends Component {
             <h2 className="myprojects-headline">My Projects</h2>
             <FontAwesomeIcon icon={faChevronDown} className="chevrondown" />
           </div>
-          <div className="notes-area"></div>
+          <div className="notes-area">
+            <div className="project">
+              <h3 className="h-project">
+                <a href="https://emretok.dev/">emretok.dev</a>
+              </h3>
+              <p className="project-info">
+                This is the source code of website that you are in. It's written
+                by React and pure css.
+              </p>
+            </div>
+            <div className="project">
+              <h3 className="h-project">
+                <a href="https://github.com/emretokk/rubikscube">
+                  Rubik's Cube
+                </a>
+              </h3>
+              <p className="project-info">
+                Its a terminal based rubik's cube implementation, if you know
+                how to solve it then go ahead {":)"}
+              </p>
+            </div>
+            <div className="project">
+              <h3 className="h-project">
+                <a href="https://github.com/emretokk/30daysreact">
+                  30 Days of React
+                </a>
+              </h3>
+              <p className="project-info">
+                I learned the react with this, there is lots of mini projects in
+                this repo. Still active not finished {":("}
+              </p>
+            </div>
+            <div className="project">
+              <h3 className="h-project">
+                <a href="https://github.com/emretokk/30daysjs">
+                  30 Days of Javascript
+                </a>
+              </h3>
+              <p className="project-info">
+                I learned the Javascript with this. Its completed except just a
+                few days boring days. I'll do them someday :P
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
